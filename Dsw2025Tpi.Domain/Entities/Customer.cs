17e1e1338f62717920474e6bb0dee1891043b51a
Javier Andres Ferreyra Vagliati <Javier.FerreyraVagliati@alu.frt.utn.edu.ar>
2025-06-21 19:14:22 -0300

    Creacion de entidades del Dominio

---
+++ Dsw2025Tpi.Domain/Entities/Customer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dsw2025Tpi.Domain.Entities
+{
+    public class Customer : EntityBase
+    {
+        public string? Email { get; set; }
+        public string? Name { get; set; }
+        public string? PhoneNumber { get; set; }
+        public ICollection<Order>? Order { get; }
+    }
+}
---
+++ Dsw2025Tpi.Domain/Entities/Order.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dsw2025Tpi.Domain.Enums;
+
+namespace Dsw2025Tpi.Domain.Entities
+{
+    public class Order : EntityBase
+    {
+        public DateTime Date { get; } = DateTime.Now;
+        public string? ShippingAddress { get; set; }
+        public string? BilingAddress { get; set; }
+        public string? Notes { get; set; }
+        public decimal TotalAmount { get; set; }
+        public ICollection<OrderItem> OrderItems { get; }
+        public Customer? Customer { get; set; }
+        public OrderStatus OrderStatus { get; set; }
+        public Guid? CustomerId { get; set; }
+
+    }
+}
