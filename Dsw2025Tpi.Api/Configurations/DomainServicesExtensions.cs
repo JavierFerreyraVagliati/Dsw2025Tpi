b12c46b102c9fb24c8738bc181221c0db384e379
lucas <Lucas.ChipolariVillena@alu.frt.utn.edu.ar>
2025-07-19 23:32:39 -0300

    realizamos la autenticacion de bearer token con jwt, y el registro y login de usuario

---
+++ Dsw2025Tpi.Api/Configurations/DomainServicesExtensions.cs
@@ -0,0 +1,33 @@
+﻿using Dsw2025Tpi.Application.Services;
+using Dsw2025Tpi.Data;
+using Dsw2025Tpi.Data.Repositories;
+using Dsw2025Tpi.Domain;
+using Dsw2025Tpi.Domain.Entities;
+using Dsw2025Tpi.Data.Helpers;
+using Microsoft.EntityFrameworkCore;
+using Dsw2025Tpi.Domain.Interfaces;
+
+namespace Dsw2025Tpi.Api.Configurations;
+
+public static class DomainServicesConfigurationExtension
+{
+    public static IServiceCollection AddDomainServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<Dsw2025TpiContext>(options =>
+        {
+            options.UseSqlServer(configuration.GetConnectionString("Dsw2025TpiDb"));
+            options.UseSeeding((c, t) =>
+            {
+                ((Dsw2025TpiContext)c).Seedwork<Product>("Source\\products.json");
+                ((Dsw2025TpiContext)c).Seedwork<Customer>("Source\\customers.json");
+            });
+        });
+
+        services.AddScoped<IRepository, EfRepository>();
+        services.AddScoped<ProductsManagmentService>();
+        services.AddScoped<OrdersManagmentService>();
+        services.AddScoped<CustomerManagmentService>();
+
+        return services;
+    }
+}
--- Dsw2025Tpi.Api/Program.cs
+++ Dsw2025Tpi.Api/Program.cs
@@ -1,16 +1,16 @@
-
-using Microsoft.AspNetCore.DataProtection.Repositories;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System.Text;
+using System.Text.Json.Serialization;
 using Dsw2025Tpi.Domain.Interfaces;
+using Dsw2025Tpi.Data;
+using Dsw2025Tpi.Data.Helpers;
 using Dsw2025Tpi.Data.Repositories;
 using Dsw2025Tpi.Application.Services;
-using Dsw2025Tpi.Data;
-using Microsoft.EntityFrameworkCore;
-using System.Diagnostics.Metrics;
-using System;
-using Dsw2025Tpi.Domain.Entities;
-using Dsw2025Tpi.Data.Helpers;
-using System.Diagnostics;
-using System.Text.Json.Serialization;
+using Dsw2025Tpi.Api.Configurations;
 
 namespace Dsw2025Tpi.Api;
 
@@ -19,42 +19,117 @@ public class Program
     public static void Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
-        // Add services to the container.
-        builder.Services.AddDbContext<Dsw2025TpiContext>(options => {
-        options.UseSqlServer("Data Source = (localdb)\\MSSQLLocalDB; Initial Catalog = Dsw2025TpiDb; Integrated Security = True;");
-     options.UseSeeding((c, t) =>
-     {
-         ((Dsw2025TpiContext)c).Seedwork<Customer>("Source\\customers.json");
-    });
+
+        // Cargar configuración JWT
+        var jwtConfig = builder.Configuration.GetSection("Jwt");
+        var key = Encoding.UTF8.GetBytes(jwtConfig["Key"]);
+
+
+  
+
+        builder.Services.AddDbContext<AuthenticateContext>(options =>
+        {
+            options.UseSqlServer(builder.Configuration.GetConnectionString("Dsw2025TpiDb"));
         });
-        builder.Services.AddControllers();
-        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-        builder.Services.AddEndpointsApiExplorer();
-        builder.Services.AddSwaggerGen();
-        builder.Services.AddHealthChecks();
 
-        builder.Services.AddControllers()
-      .AddJsonOptions(options =>
-      {
-          options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
-      });
+        // Agregar Identity
+        builder.Services.AddIdentity<IdentityUser, IdentityRole>(options =>
+        {
+            options.Password = new PasswordOptions
+            {
+                RequiredLength = 8,
+                RequireDigit = true,
+                RequireNonAlphanumeric = false,
+                RequireUppercase = true,
+                RequireLowercase = true,
+            };
+        })
+        .AddEntityFrameworkStores<AuthenticateContext>()
+        .AddDefaultTokenProviders();
 
+        // Autenticación JWT
+        builder.Services.AddAuthentication(options =>
+        {
+            options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        })
+        .AddJwtBearer(options =>
+        {
+            options.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuer = true,
+                ValidateAudience = true,
+                ValidateLifetime = true,
+                ValidateIssuerSigningKey = true,
+                ValidIssuer = jwtConfig["Issuer"],
+                ValidAudience = jwtConfig["Audience"],
+                IssuerSigningKey = new SymmetricSecurityKey(key)
+            };
+        });
+
+        // CORS
+        builder.Services.AddCors(options =>
+        {
+            options.AddPolicy("PermitirFrontend", policy =>
+                policy.WithOrigins("http://localhost:3000")
+                      .AllowAnyHeader()
+                      .AllowAnyMethod());
+        });
+
+        // Servicios propios
         builder.Services.AddTransient<IRepository, EfRepository>();
         builder.Services.AddScoped<ProductsManagmentService>();
-        builder.Services.AddScoped<OrdersManagmentService>();
-        builder.Services.AddScoped<CustomerManagmentService>();
+        builder.Services.AddSingleton<JwtTokenService>();
+        builder.Services.AddDomainServices(builder.Configuration);
 
+        // Controladores
+        builder.Services.AddControllers()
+            .AddJsonOptions(options =>
+            {
+                options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+            });
+
+        // Swagger con JWT
+        builder.Services.AddEndpointsApiExplorer();
         builder.Services.AddSwaggerGen(c =>
         {
+            c.SwaggerDoc("v1", new OpenApiInfo
+            {
+                Title = "Dsw2025Tpi API",
+                Version = "v1"
+            });
+
+            c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+            {
+                In = ParameterLocation.Header,
+                Name = "Authorization",
+                Description = "Ingrese el token JWT con el prefijo 'Bearer '",
+                Type = SecuritySchemeType.ApiKey
+            });
+
+            c.AddSecurityRequirement(new OpenApiSecurityRequirement
+            {
+                {
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference
+                        {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = "Bearer"
+                        }
+                    },
+                    Array.Empty<string>()
+                }
+            });
+
             c.CustomSchemaIds(type => type.FullName!.Replace("+", "."));
         });
-   
-     
+
+        builder.Services.AddHealthChecks();
 
         var app = builder.Build();
 
-
-        // Configure the HTTP request pipeline.
+        // Middlewares
         if (app.Environment.IsDevelopment())
         {
             app.UseSwagger();
@@ -63,10 +138,12 @@ public class Program
 
         app.UseHttpsRedirection();
 
+        app.UseCors("PermitirFrontend");
+
+        app.UseAuthentication();
         app.UseAuthorization();
 
         app.MapControllers();
-        
         app.MapHealthChecks("/healthcheck");
 
         app.Run();
